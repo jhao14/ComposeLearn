989a94f416e71c91be60406736e919ed72ade88d
jianghao <18772101289@163.com>
2023-11-05 17:00:58 +0800

    布局小实战

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -11,6 +11,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -19,6 +22,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 
@@ -31,7 +35,13 @@ class MainActivity : ComponentActivity() {
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+//                    Greeting("Android")
+                Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+                    for (i in 0..20){
+                        More("Compose课程第${i+1}课,快来学习吧～")
+                    }
+                }
+
                 }
             }
         }
@@ -52,15 +62,32 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                 .padding(10.dp)
                 .clickable {
                     Log.i(TAG, "Greeting: click")
-                }
+                },
+            color = Color.White
             )
     }
 }
+@Composable
+fun More(title:String){
+    Row(modifier= Modifier
+        .background(Color.Red)
+        .padding(10.dp)) {
+        Text(text = title,
+            fontSize = 16.sp,color=Color.White, modifier = Modifier.weight(1f))
+        Button(onClick = {
+            Log.i(TAG, "More: ")
+        }){
+            Text(text = "查看详情",color= Color.White)
+        }
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyApplicationTheme {
-        Greeting("Android")
+//        Greeting("Android")
+    More("Hello Compose")
     }
 }
